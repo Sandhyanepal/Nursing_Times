7674e245127d888a924782dd2ebd66b953dbcc14
shriyabhandari <shriya2001bhandari@gmail.com>
2024-04-09 21:22:23 +0545

    dashupdate

--- frontend/src/components/AdminSidebar.js
+++ frontend/src/components/AdminSidebar.js
@@ -6,15 +6,22 @@ const AdminSidebar = () => {
 
   return (
     <>
-    <div className='w-1/4 p-4 bg-lime-700 text-white font-bold text-2xl flex flex-col justify-between font-serif' style={{minHeight:'100vh', minWidth:'25%'}}>
+    <div className='w-1/5 p-4 bg-amber-100 text-black font-bold text-xl flex flex-col justify-between font-serif' style={{minHeight:'100vh', minWidth:'25%'}}>
         <ul>
-            <li><Link href={'/admindashboard'}>Admin Dashboard</Link></li>
-            <li><Link href={'/posts'}>Posts</Link></li>
-            <li><Link href={'/category'}>Category</Link></li>
-            <li><Link href={'/users'}>Users</Link></li>
+            <li className='text-gray-600 text-2xl'><Link href={''}>Nursing Times</Link></li>
+            <li><Link href={''}>Users</Link></li>
+            <li><Link href={'/contact'}>Posts</Link></li>
+            <li><Link href={''}>Category</Link></li>
+            <li><Link href={''}>Notifications</Link></li>
         </ul>
-        <button className='bg-white text-orange-600'>Sign Out</button>
+        {/* <h4 className='text-orange-400 pt-3'>Personal Information</h4>
+        <ul>
+        <li><Link href={'/users'}>Users</Link></li>
+        </ul> */}
+
+        <button className='bg-white text-orange-600'>Log Out</button>
     </div>
+   
     </>
   )
 }

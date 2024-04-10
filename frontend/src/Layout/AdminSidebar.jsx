54996a03511c632bd77e438d43e211acd27a1703
shriyabhandari <shriya2001bhandari@gmail.com>
2024-04-10 17:02:07 +0545

    categorymain

--- frontend/src/Layout/AdminSidebar.jsx
+++ frontend/src/Layout/AdminSidebar.jsx
@@ -6,13 +6,16 @@ const AdminSidebar = () => {
   
 
   return (
-    <div className='w-1/4 p-5 bg-gray-400'>
+    <div className='w-1/5 p-4 bg-gray-400' style={{minHeight: '100vh'}}>
       <Link to='/admindashboard' className='text-center text-white font-bold text-xl'>Admin Dashboard</Link>
       <ul className='text-white font-bold'>
         <li><Link to='/posts' >Posts</Link></li>
-        <li><Link to='/category'>Category</Link></li>
+        <li><Link to='/admin/category'>Category</Link></li>
         <li><Link to='/users'>Users</Link></li>
       </ul>
+
+      {/* <button className='text-orange-600'>Log Out</button> */}
+
     </div>
 
     
--- frontend/src/api/categoryApi.js
+++ frontend/src/api/categoryApi.js
@@ -2,6 +2,7 @@ import { API } from "../config"
 
 export const getAllCategory = () => {
     return fetch(`${API}/getAllCategory`)
-    .then(res=> res.json())
+    .then(res=>res.json())
     .catch(err=>console.log(err))
 }
+

85d8fc11a1f712ee0ffda27a1f3621a225f12287
Sandhyanepal <sandhyanepal99@gmail.com>
2024-04-25 17:15:44 +0545

    Dashboard

--- frontend/src/Layout/AdminProfile.jsx
+++ frontend/src/Layout/AdminProfile.jsx
@@ -2,8 +2,58 @@ import React from 'react'
 
 const AdminProfile = () => {
   return (
-    <div>
-      <h1 className=''>Dashboard</h1>
+    <div className='ml-12 w-full mr-12'>
+
+      <h1 className='text-3xl font-bold mt-8 '>Dashboard</h1>
+      <div className='adminprofile-grid grid gap-3 my-14'>
+
+        <div className='box1 p-5 border-2 rounded-md '>
+            <i class="fa-solid fa-users pr-3 text-2xl"></i>
+            <span className='font-semibold text-2xl'>Total Users</span>
+            <h1 className='py-6 text-xl'>11.22</h1>
+          <p >view all</p>
+        </div>
+
+        <div className='box2 p-5 border-2 rounded-md '>
+          <i className="fa-regular fa-newspaper pr-3 text-2xl"></i>
+          <span className='font-semibold text-2xl'>Total Posts</span>
+          <h1 className='py-6 text-xl'>11.22</h1>
+          <p >view all</p>
+        </div>
+
+        <div className='box3 p-5 border-2 rounded-md '>
+          <i class="fa-regular fa-heart pr-3 text-2xl"></i>
+          <span className='font-semibold text-2xl'>Total Likes</span>
+          <h1 className='py-6 text-xl'>11.22</h1>
+          <p >view all</p>
+        </div>
+
+        <div className='box4 p-5 border-2 rounded-md '>
+          <i class="fa-solid fa-comments pr-3 text-2xl"></i>
+          <span className='font-semibold text-2xl'>Total Comments</span>
+          <h1 className='py-6 text-xl'>11.22</h1>
+          <p >view all</p>
+        </div>
+
+        <div className='box5 p-5 border-2 rounded-md '>
+          <i className="fa-solid fa-eye pr-3 text-2xl"></i>
+          <span className='font-semibold text-2xl'>Total Views</span>
+          <h1 className='py-6 text-xl'>11.22</h1>
+          <p >view all</p>
+        </div>
+
+        <div className='box6 p-5 border-2 rounded-md'>Box5</div>
+        <div className='box7 p-5 border-2 rounded-md'>Box6</div>
+        <div className='box8 p-5 border-2 rounded-md'>Box7</div>
+        <div className='box9 p-5 border-2 rounded-md'>Box8</div>
+      </div>
+
+      {/* <div className="insights">
+        <div className="users">
+        <i className="fa-solid fa-chart-simple"></i>
+        </div>
+        <div className="posts"></div>
+      </div> */}
     </div>
   )
 }
--- frontend/src/Layout/AdminSidebar.jsx
+++ frontend/src/Layout/AdminSidebar.jsx
@@ -6,12 +6,12 @@ const AdminSidebar = () => {
 
 
   return (
-    <div className=' bg-gray-300 relative' style={{height:"100vh"}}>
+    <div className=' bg-gray-300 relative'>
 
 
       <div className='topbar flex gap-8 m-8'>
 
-        <div className="logo font-bold text-3xl">Nursing<span className='text-yellow-500'>Times</span></div>
+        <Link to='/' className="logo font-bold text-3xl">Nursing<span className='text-yellow-500'>Times</span></Link>
 
         <div className='close text-3xl hidden' id='close-btn'>
           <i className="fa-solid fa-xmark"></i>
@@ -24,6 +24,11 @@ const AdminSidebar = () => {
 
         <ul className='m-8'>
           
+          <li><Link to='/' className='flex items-center gap-3 my-4'>
+          <i className="fa-solid fa-house"></i>           
+          <h1>Homepage</h1>
+          </Link></li>
+
           <li><Link to='/users' className='flex items-center gap-3 my-4'>
           <i className="fa-regular fa-user"/>           
           <h1>Users</h1>
@@ -44,7 +49,12 @@ const AdminSidebar = () => {
             <h1>Category</h1>
           </Link></li>
 
-          <li><Link to='/admin/category' className='flex items-center gap-3 my-4 absolute bottom-8'>
+          <li><Link to='' className='flex items-center gap-3 my-4'>
+          <i className="fa-solid fa-gear"></i>
+            <h1>Settings</h1>
+          </Link></li>
+
+          <li><Link to='' className='flex items-center gap-3 my-4 absolute bottom-8'>
           <i className="fa-solid fa-right-from-bracket"/>
             <h1>Logout</h1>
           </Link></li>

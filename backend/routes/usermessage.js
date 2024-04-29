cdcd4222248c968cc77befce338dbc8b9da9b7a0
Sandhyanepal <sandhyanepal99@gmail.com>
2024-04-29 17:10:57 +0545

    user message

--- backend/routes/usermessage.js
+++ backend/routes/usermessage.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { submitUserMessage } = require('../controller/usermessage')
 const router = express.Router()
 
 router.post('/submitusermessage', submitUserMessage)
